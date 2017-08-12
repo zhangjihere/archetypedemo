0e1c417f1fa3b2e52a277ad6cb0a1bc9c0ef821d
Ji Zhang <ji.zhang@samsung.com>
2017-08-12 13:40:22 +0800

    tzb/ 扩展CompletionServiceTest示例

--- tzb/src/main/java/chapter05/tools/CompletionServiceTest.java
+++ tzb/src/main/java/chapter05/tools/CompletionServiceTest.java
@@ -17,6 +17,7 @@ public class CompletionServiceTest {
 		CompletionService<String>completionService = new ExecutorCompletionService<String>(executorService);
 		for(int i = 0 ; i < 100 ; i++) {
 			final int num = i;
+			//ExecutorCompletionService.submit是可以sumbit完成所有任务（Callable）之后再方便获取Future对象
 			completionService.submit(new Callable<String>() {
 				public String call() {
 					try {
@@ -27,11 +28,40 @@ public class CompletionServiceTest {
 					return "a" + num;
 				}
 			});
-		}
+			//直接执行任务Runnable
+			executorService.execute(new Runnable() {
+				@Override
+				public void run() {
+					try {
+						Thread.sleep((random.nextLong()) & 5000);
+						System.out.println("run empty");
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+			});
+			//ExecutorService.submit任务（Callable）立即返回Future来使用
+            Future<String> normalSubmit = executorService.submit(new Callable<String>() {
+                @Override
+                public String call() throws Exception {
+                    try {
+                        Thread.sleep((random.nextLong()) & 5000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    return "b" + num;
+                }
+            });
+        }
+        /**
+         * 特别留意，同一个ExecutorService实例提交的任务，与使用其实例化的CompletionService实例提交的任务，相互独立。
+         * 即两者共用的线程池，CompletionService随后由take或poll的任务结果与ExecutorService的execute或submit的任务结果无关
+         */
 		for(int i = 0 ; i < 100 ; i++) {
 			Future<String> f = completionService.take();
 			System.out.println(f.get());
 		}
-		executorService.shutdown();
+
+        executorService.shutdown();
 	}
 }
