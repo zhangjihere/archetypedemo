f2025a44717cced35bb06e508db51a3e35a95aa0
Ji Zhang <ji.zhang@samsung.com>
2017-08-21 11:27:37 +0800

    guava-demo/ modify BloomFilter demo，枚举单例和普通实例

--- guava-demo/src/main/java/org/tombear/demo/guava/oddsends/BloomFilterClass.java
+++ guava-demo/src/main/java/org/tombear/demo/guava/oddsends/BloomFilterClass.java
@@ -19,17 +19,22 @@ public class BloomFilterClass {
         List<Book> books = Book.createBookList();
 
         BloomFilter<Book> bloomFilter = BloomFilter.create(BookFunnel.FUNNEL, 5);
+        BloomFilter<Book> bloomFilter2 = BloomFilter.create(new BookFunnel2(), 5);
         for (Book book : books) {
             bloomFilter.put(book);
         }
 
         Book newBook = new Book();
-        newBook.setTitle("Mountain Climbing");
+        newBook.setTitle("Apple Cook");
+        newBook.setIsbn("9527-1");
         Book book1 = books.get(0);
         System.out.println("book " + book1.getTitle() + " contained " + bloomFilter.mightContain(book1));
         System.out.println("book " + newBook.getTitle() + " contained " + bloomFilter.mightContain(newBook));
     }
 
+    /**
+     * 枚举类实现Funnel实例的单例，可序列化等特性
+     */
     enum BookFunnel implements Funnel<Book> {
         //This is the single enum value
         FUNNEL;
@@ -39,5 +44,16 @@ public class BloomFilterClass {
                     .putDouble(from.getPrice());
         }
 
+    }
+
+    /**
+     * 声明静态类，非单例，也未标示序列化接口
+     */
+    static class BookFunnel2 implements Funnel<Book> {
+        @Override
+        public void funnel(Book book, PrimitiveSink primitiveSink) {
+            primitiveSink.putBytes(book.getIsbn().getBytes(Charsets.UTF_8))
+                    .putDouble(book.getPrice());
+        }
     }
 }
