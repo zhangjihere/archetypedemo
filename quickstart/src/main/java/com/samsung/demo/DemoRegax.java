d3325dcf7dbb9b1eab3acf34847bfe383236ee2c
Ji Zhang <ji.zhang@samsung.com>
2018-07-09 17:55:23 +0800

    update regex demo

--- quickstart/src/main/java/com/samsung/demo/DemoRegax.java
+++ quickstart/src/main/java/com/samsung/demo/DemoRegax.java
@@ -9,7 +9,7 @@ import java.util.regex.Pattern;
 public class DemoRegax {
     public static void main(String[] args) {
         Pattern p = Pattern.compile("(\\d{1,2})");
-        Matcher m = p.matcher("12 54 1 65");
+        Matcher m = p.matcher("12,54,1,65");
         StringBuffer s = new StringBuffer();
         while (m.find())
             m.appendReplacement(s, String.valueOf(3 * Integer.parseInt(m.group(1))));
